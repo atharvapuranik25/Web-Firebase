bfadceb9af7ccc722c35b88c658958d95289fabc
Atharva Puranik <atharva.puranik@avantika.edu.in>
2022-05-30 22:12:41 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA3WrqCTjT3J3LKjcm3n-l2Z7jxU9xwzJE",
+  authDomain: "crudwithfirebase-a6e73.firebaseapp.com",
+  databaseURL: "https://crudwithfirebase-a6e73-default-rtdb.firebaseio.com",
+  projectId: "crudwithfirebase-a6e73",
+  storageBucket: "crudwithfirebase-a6e73.appspot.com",
+  messagingSenderId: "67089238171",
+  appId: "1:67089238171:web:edd306f606cdef4f5ede3e",
+  measurementId: "G-RKFY2ZQS2K"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
